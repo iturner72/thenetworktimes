0779444352b96cc8eee26864c4c0c65f22365a65
iturner72 <ian96turner@gmail.com>
2024-06-21 05:57:48 -0600

    update project name

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
     <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo">
 </picture>
 
-# nwtr
+# thenetworktimes
 
 this is a webapp built with the [Leptos](https://github.com/leptos-rs/leptos) web framework and the [cargo-leptos](https://github.com/akesson/cargo-leptos) tool using [Axum](https://github.com/tokio-rs/axum).
--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -5,7 +5,7 @@ use leptos_router::A;
 pub fn Navbar() -> impl IntoView {
     view! {
         <div class="flex justify-between items-center ib bg-salmon-500 px-6 py-4 bg-teal-800">
-            <A href="/" class="text-2xl text-salmon-400 hover:text-salmon-600">"nwtr"</A>
+            <A href="/" class="text-2xl text-salmon-400 hover:text-salmon-600">"thenetworktimes"</A>
             <A href="/channels" class="text-2xl text-salmon-400 hover:text-salmon-600">"channels"</A>
             <A href="/settings" class="text-2xl text-salmon-400 hover:text-salmon-600">"settings"</A>
         </div>
