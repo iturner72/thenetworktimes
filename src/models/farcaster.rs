29f3a93c99988f0ae7e7d67363be05ad12ce6ea3
iturner72 <ian96turner@gmail.com>
2024-06-27 21:55:24 -0700

    add farcaster models (these are likely wrong and will change)

--- src/models/farcaster.rs
+++ src/models/farcaster.rs
@@ -1,49 +1,109 @@
-use cfg_if::cfg_if;
-cfg_if! { if #[cfg(feature = "ssr")] {
-    use serde::{Deserialize, Serialize};
-    
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Cast {
-        pub fid: u64,
-        pub hash: String,
-        pub parent_hash: Option<String>,
-        pub author_fid: u64,
-        pub timestamp: u64,
-        pub text: String,
-        pub mentions: Vec<u64>,
-        pub mentions_positions: Vec<u32>,
-        pub embeds: Vec<Embed>,
-    }
-    
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct User {
-        pub fid: u64,
-        pub username: String,
-        pub display_name: String,
-        pub bio: String,
-        pub avatar_url: String,
-        pub verified: bool,
-    }
-    
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Reaction {
-        pub fid: u64,
-        pub hash: String,
-        pub author_fid: u64,
-        pub target_hash: String,
-        pub timestamp: u64,
-        pub reaction_type: String,
-    }
-    
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Embed {
-        pub url: Option<String>,
-        pub cast_id: Option<CastId>,
-    }
-    
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct CastId {
-        pub fid: u64,
-        pub hash: String,
-    }
-}}
+use serde::{Deserialize, Serialize};
+
+// casts
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Cast {
+    pub data: CastData,
+    pub hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CastData {
+    pub cast_add_body: Option<CastAddBody>,
+    pub fid: u64,
+    pub network: String,
+    pub timestamp: u64,
+    #[serde(rename = "type")]
+    pub cast_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CastAddBody {
+    pub embeds: Vec<Embed>,
+    pub embeds_deprecated: Vec<String>,
+    pub mentions: Vec<u64>,
+    pub mentions_positions: Vec<u32>,
+    pub parent_cast_id: Option<ParentCastId>,
+    pub text: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Embed {
+    pub url: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ParentCastId  {
+    pub fid: u64,
+    pub hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CastResponse {
+    pub messages: Vec<Cast>,
+}
+
+// user
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UserDataResponse {
+    pub data: UserData,
+    pub hash: String,
+    pub hash_scheme: String,
+    pub signature: String,
+    pub signature_scheme: String,
+    pub signer: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UserData {
+    pub fid: u64,
+    pub network: String,
+    pub timestamp: i64,
+    #[serde(rename = "type")]
+    pub data_type: String,
+    pub user_data_body: UserDataBody,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UserDataBody {
+    #[serde(rename = "type")]
+    pub data_type: String,
+    pub value: String,
+}
+
+// reactions
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ReactionResponse {
+    pub fid: u64,
+    pub hash: String,
+    pub author_fid: u64,
+    pub target_hash: String,
+    pub timestamp: u64,
+    pub reaction_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ReactionData {
+    pub fid: u64,
+    pub network: String,
+    pub reaction_body: ReactionBody,
+    pub timestamp: i64,
+    #[serde(rename = "type")]
+    pub reaction_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ReactionBody {
+    pub target_cast_id: TargetCastId,
+    #[serde(rename = "type")]
+    pub reaction_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TargetCastId {
+    pub fid: u64,
+    pub hash: String,
+}
