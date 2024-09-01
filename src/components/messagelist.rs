d367d1e24b37ff838939b07f8a2fc19b2a785537
iturner72 <ian96turner@gmail.com>
2024-08-31 19:30:16 -0700

    bump js-sys and web-sys

--- src/components/messagelist.rs
+++ src/components/messagelist.rs
@@ -29,73 +29,97 @@ pub fn MessageList(
 
     view! {
         <div class="message-list h-108 md:h-172 space-y-8 overflow-hidden hover:overflow-y-auto flex flex-col">
-            <For 
-                each=move || message_list.get().into_iter()
-                    .filter(move |message: &MessageView| {
-                        if current_thread_id.get().is_empty() {
-                            true
-                        } else {
-                            message.thread_id == Some(current_thread_id.get())
-                        }
-                    })
-                key=|message| message.id.clone()
-                children=move|message| {
+            <For
+                each=move || {
+                    message_list
+                        .get()
+                        .into_iter()
+                        .filter(move |message: &MessageView| {
+                            if current_thread_id.get().is_empty() {
+                                true
+                            } else {
+                                message.thread_id == Some(current_thread_id.get())
+                            }
+                        })
+                }
+
+                key=|message| message.id
+                children=move |message| {
                     view! {
-                        <div class={format!("message-wrapper flex w-full {}", if message.role == "assistant" { "justify-start" } else { "justify-end" })}>
-                            <button class={format!("message-item border-2 p-2 hover:border-teal-800 bg-teal-800 hover:bg-teal-900 transition duration-0 group {}",
-                                        if message.role == "assistant" {
-                                            "border-none bg-opacity-0 self-start" 
-                                        } else { 
-                                            "border-teal-700 bg-teal-800 self-end" 
-                                    })}
-                                    on:click={move |_| {
-                                        let element = window().unwrap().document().unwrap()
-                                            .query_selector_all(".info-for-nerds").unwrap();
-                                        for i in 0..element.length() {
-                                            let item = element.item(i).unwrap().dyn_into::<Element>().unwrap();
-                                            item.class_list().toggle("hidden").unwrap();
-                                        }
-                                    }}
+                        <div class=format!(
+                            "message-wrapper flex w-full {}",
+                            if message.role == "assistant" {
+                                "justify-start"
+                            } else {
+                                "justify-end"
+                            },
+                        )>
+                            <button
+                                class=format!(
+                                    "message-item border-2 p-2 hover:border-teal-800 bg-teal-800 hover:bg-teal-900 transition duration-0 group {}",
+                                    if message.role == "assistant" {
+                                        "border-none bg-opacity-0 self-start"
+                                    } else {
+                                        "border-teal-700 bg-teal-800 self-end"
+                                    },
+                                )
 
-                           /* 
-                            * this button is insane, i know... 
-                            *
-                            * (here's a 3 bullet breakdown :O)
-                            * 
-                            *  • button uses web_sys to access the DOM. It 
-                            *  retrieves all elements with the class 
-                            *  "info-for-nerds" using query_selector_all.
-                            *  
-                            *  • A loop iterates through the NodeList, 
-                            *  converting each item to an Element. This 
-                            *  allows manipulation of individual elements.
-                            *  
-                            *  • The code toggles the "hidden" class on each 
-                            *  element using class_list().toggle(). This 
-                            *  shows/hides additional message info on hover.
-                            * 
-                            * not so bad :)
-                            *
-                            */
-
+                                on:click=move |_| {
+                                    let element = window()
+                                        .unwrap()
+                                        .document()
+                                        .unwrap()
+                                        .query_selector_all(".info-for-nerds")
+                                        .unwrap();
+                                    for i in 0..element.length() {
+                                        let item = element
+                                            .item(i)
+                                            .unwrap()
+                                            .dyn_into::<Element>()
+                                            .unwrap();
+                                        item.class_list().toggle("hidden").unwrap();
+                                    }
+                                }
                             >
+
                                 <div class="flex flex-row items-center space-x-2">
-                                    <img src="openai_square_logo.webp" class="w-6 h-6 rounded-full"/>
-                                    <img src="anthropic_square_logo.webp" class="w-6 h-6 rounded-full"/>
-                                    <p class="message-content ir text-base text-gray-700 hover:text-teal-400">{message.content.clone()}</p>
+                                    <img
+                                        src="openai_square_logo.webp"
+                                        class="w-6 h-6 rounded-full"
+                                    />
+                                    <img
+                                        src="anthropic_square_logo.webp"
+                                        class="w-6 h-6 rounded-full"
+                                    />
+                                    <p class="message-content ir text-base text-gray-700 hover:text-teal-400">
+                                        {message.content.clone()}
+                                    </p>
                                 </div>
                                 <div class="info-for-nerds flex flex-row justify-between space-x-12 pt-8 hidden">
                                     <div class="ai-info flex flex-col space-y-1">
-                                        <p class="message-thread_id ir text-xs text-gray-800 hover:text-gray-600">thread id: {message.thread_id.clone()}</p>
-                                        <p class="message-id ir text-xs text-gray-800 hover:text-gray-600">message id: {message.id.clone()}</p>
+                                        <p class="message-thread_id ir text-xs text-gray-800 hover:text-gray-600">
+                                            thread id: {message.thread_id.clone()}
+                                        </p>
+                                        <p class="message-id ir text-xs text-gray-800 hover:text-gray-600">
+                                            message id: {message.id}
+                                        </p>
                                         <p class="message-created_at ir text-xs text-gray-900 hover:text-gray-700">
-                                            {message.created_at.map(|dt| dt.format("%b %d, %I:%M %p").to_string()).unwrap_or_default()}
+                                            {message
+                                                .created_at
+                                                .map(|dt| dt.format("%b %d, %I:%M %p").to_string())
+                                                .unwrap_or_default()}
                                         </p>
                                     </div>
                                     <div class="message-info flex flex-col space-y-1">
-                                        <p class="message-role ir text-xs text-gray-600 hover:text-salmon-400">role: {message.role.clone()}</p>
-                                        <p class="message-active_lab ir text-xs text-aqua-700 hover:text-aqua-400">lab: {message.active_lab.clone()}</p>
-                                        <p class="message-active_model ib text-xs text-mint-600 hover:text-mint-700">model: {message.active_model.clone()}</p>
+                                        <p class="message-role ir text-xs text-gray-600 hover:text-salmon-400">
+                                            role: {message.role.clone()}
+                                        </p>
+                                        <p class="message-active_lab ir text-xs text-aqua-700 hover:text-aqua-400">
+                                            lab: {message.active_lab.clone()}
+                                        </p>
+                                        <p class="message-active_model ib text-xs text-mint-600 hover:text-mint-700">
+                                            model: {message.active_model.clone()}
+                                        </p>
                                     </div>
                                 </div>
                             </button>
@@ -103,6 +127,7 @@ pub fn MessageList(
                     }
                 }
             />
+
         </div>
     }
 }
@@ -118,17 +143,17 @@ pub async fn get_messages() -> Result<Vec<MessageView>, ServerFnError> {
 
     #[derive(Debug)]
     enum MessageError {
-        PoolError(String),
-        DatabaseError(diesel::result::Error),
-        InteractionError(String),
+        Pool(String),
+        Database(diesel::result::Error),
+        Interaction(String),
     }
 
     impl fmt::Display for MessageError {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             match self {
-                MessageError::PoolError(e) => write!(f, "Pool error: {}", e),
-                MessageError::DatabaseError(e) => write!(f, "Database error: {}", e),
-                MessageError::InteractionError(e) => write!(f, "Interaction error: {}", e),
+                MessageError::Pool(e) => write!(f, "Pool error: {}", e),
+                MessageError::Database(e) => write!(f, "Database error: {}", e),
+                MessageError::Interaction(e) => write!(f, "Interaction error: {}", e),
             }
         }
     }
@@ -145,15 +170,15 @@ pub async fn get_messages() -> Result<Vec<MessageView>, ServerFnError> {
     let conn = pool
         .get()
         .await
-        .map_err(|e| MessageError::PoolError(e.to_string()))
+        .map_err(|e| MessageError::Pool(e.to_string()))
         .map_err(to_server_error)?;
 
     let result = conn
         .interact(|conn| messages_table.load::<Message>(conn))
         .await
-        .map_err(|e| MessageError::InteractionError(e.to_string()))
+        .map_err(|e| MessageError::Interaction(e.to_string()))
         .map_err(to_server_error)?
-        .map_err(MessageError::DatabaseError)
+        .map_err(MessageError::Database)
         .map_err(to_server_error)?;
 
     Ok(result.into_iter().map(MessageView::from).collect())
