0c070cabf9424e892fe65903bcdc3ab314fbeb50
Ian Turner <iturner.eth@Ians-MacBook-Air-2.local>
2024-09-03 07:28:28 -0600

    idk

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -7,10 +7,10 @@ pub fn Navbar() -> impl IntoView {
         <div class="flex justify-between items-center ib bg-salmon-500 px-6 py-4 bg-teal-800">
             <A href="/" class="text-2xl text-salmon-400 hover:text-salmon-600">"nwt"</A>
             <A href="/feed" class="text-2xl text-wenge-300 hover:text-mint-700">"feed"</A>
-            <A href="/writersroom" class="text-2xl text-salmon-400 hover:text-salmon-600">"writersroom"</A>
-            <A href="/settings" class="text-2xl text-salmon-400 hover:text-salmon-600">"settings"</A>
-            <A href="/codedemo" class="text-2xl text-gray-700 hover:text-salmon-600">"highlightjs"</A>
-            <A href="/mermaiddemo" class="text-2xl text-mint-700 hover:text-salmon-600">"mermaidchart"</A>
+            <A href="/writersroom" class="text-2xl text-salmon-400 hover:text-salmon-600">"yap"</A>
+            <A href="/settings" class="text-2xl text-salmon-400 hover:text-salmon-600">"advanced"</A>
+            <A href="/codedemo" class="text-2xl text-gray-700 hover:text-salmon-600">"hljs"</A>
+            <A href="/mermaiddemo" class="text-2xl text-mint-700 hover:text-salmon-600">"mermaid"</A>
         </div>
     }
 }
